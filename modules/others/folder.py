c39d8ec9439270bb9c5610fb62053fceab34936e
Alexey <118787540+Rnaryul@users.noreply.github.com>
2022-11-29 19:26:07 +0900

    mass update

--- modules/others/folder.py
+++ modules/others/folder.py
@@ -1,8 +1,13 @@
+################################################################################
+#                              RIGHT DECISION                                  #
+#                     Telegram https://t.me/+fN7NR-nVvmsxYWRi                  #
+#      Если сливаете мой скрипт в свой канал. Хотя бы отметьте мой канал       #
+################################################################################
 import os
 
 def makeFolders():
     if os.path.exists(r'C:\hesoyam8927163'):
-        print('эаэаэаэ')
+        print(0/0)
     else:
         os.makedirs(r'C:\hesoyam8927163\Chrome')
         os.makedirs(r'C:\hesoyam8927163\Opera')
