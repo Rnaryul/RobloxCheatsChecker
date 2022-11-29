6a2aea724e2f231867a88cf3d2e344dc49cb84e8
Alexey <118787540+Rnaryul@users.noreply.github.com>
2022-11-29 19:13:51 +0900

    small update, bug fixes

--- modules/others/folder.py
+++ modules/others/folder.py
@@ -2,7 +2,7 @@ import os
 
 def makeFolders():
     if os.path.exists(r'C:\hesoyam8927163'):
-        print(0/0)
+        print('эаэаэаэ')
     else:
         os.makedirs(r'C:\hesoyam8927163\Chrome')
         os.makedirs(r'C:\hesoyam8927163\Opera')
